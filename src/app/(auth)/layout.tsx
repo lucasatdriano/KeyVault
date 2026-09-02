70813dca6f8b41733aaa8e63259b3e761d0fe1a5
lucasatdriano <lucasatdriano@gmail.com>
2026-09-02 05:35:55 -0300

    fix: update minimum height class for AuthLayout component

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
     return (
-        <div className="min-h-screen bg-background flex flex-col items-center justify-center px-6 relative overflow-hidden">
+        <div className="min-h-dvh bg-background flex flex-col items-center justify-center px-6 relative overflow-hidden">
             <div className="absolute -top-25 -right-25 w-64 h-64 bg-primary/20 rounded-full opacity-20 blur-3xl"></div>
             <div className="absolute -bottom-25 -left-25 w-80 h-80 bg-secondary/20 rounded-full opacity-20 blur-3xl"></div>
 
